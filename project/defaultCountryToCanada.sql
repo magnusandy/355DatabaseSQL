71220a7024f5c4131f6540b18bdb4e360a3d3ce5
Iain Workman <ipw969@peon17.usask.ca>
2014-11-26 13:22:09 -0600

    Renamed defaultCountryToCanada to defaultCountryToCanada.sql, fixed syntax error in setting default value

--- project/defaultCountryToCanada.sql
+++ project/defaultCountryToCanada.sql
@@ -4,4 +4,4 @@ BEGIN;
 UPDATE t_clients SET cl_country = 'Canada' WHERE cl_country IS NULL;
 
 -- Add default value of 'Canada' to country domain
-ALTER DOMAIN country SET DEFAULT VALUE = 'Canada';
+ALTER DOMAIN country SET DEFAULT 'Canada';
