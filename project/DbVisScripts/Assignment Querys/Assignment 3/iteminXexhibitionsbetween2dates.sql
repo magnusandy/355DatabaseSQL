a712d267f24d0ec8f3b15d3e96572696c7685be7
knames <okenso@gmail.com>
2014-11-28 16:35:16 -0600

    Asn3: Query: Query lists all exhibitions item X is in between date Y and Z
    Assignment 3
     Query lists all exhibitions item X is in between date Y and Z

---
+++ project/DbVisScripts/Assignment Querys/Assignment 3/iteminXexhibitionsbetween2dates.sql
@@ -0,0 +1,10 @@
+select 
+        exi_ename, exi_showdate_start, exi_exidate_start, exi_exidate_end
+FROM
+        t_exhibition_items
+WHERE
+        exi_inumkey = ${itemNum||(null)||Float}$ AND
+        exi_ialphakey = ${itemAlpha||(null)||String}$ AND
+        exi_exidate_start >= ${startdate||(null)||Date}$ AND
+        exi_exidate_end <= ${enddate||(null)||Date}$
+;
