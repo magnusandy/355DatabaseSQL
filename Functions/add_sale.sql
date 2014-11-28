f973bf59ca3f4026e3389c774eda0aee9faa147e
Iain Workman <ipw969@tux2.usask.ca>
2014-11-28 10:00:02 -0600

    Got all the preconds of add_sale setup

--- Functions/add_sale.sql
+++ Functions/add_sale.sql
@@ -1,19 +1,22 @@
-BEGIN;
-CREATE FUNCTION sell_item(item_number real, item_code varchar(50), client varhcar(50)) RETURNS VOID AS $f_sellitem$
+CREATE OR REPLACE FUNCTION sell_item(item_number inumkey, item_code ialphakey, client clname, sale_date itdatetime) RETURNS VOID AS $f_sellitem$
 DECLARE
 current_owner clname;
 BEGIN
-	-- Check if passed variables are not null
+	-- Ensure passed variables are not null
         IF(item_number IS NULL) THEN
-		RAISE EXCEPTION 'Unable to sell an item with no item_number';
+		RAISE EXCEPTION 'Unable to sell an item with an unknown item_number';
 	END IF;
 
 	IF(item_code IS NULL) THEN
-		RAISE EXCEPTION 'Unable to sell an item with no item_code';
+		RAISE EXCEPTION 'Unable to sell an item with an unknown item_code';
 	END IF;
 
 	IF(client IS NULL) THEN
-		RAISE EXCEPTION 'Unable to sell and item with no client';
+		RAISE EXCEPTION 'Unable to sell item % % to an unknown client', item_number, item_code;
+	END IF;
+
+	IF(sale_date IS NULL) THEN
+		RAISE EXCEPTION 'Unable to sell item % % to % on an unknown date', item_number, item_code, client;
 	END IF;
 
 	-- Ensure the item specified actually exists
@@ -21,11 +24,28 @@ BEGIN
 		RAISE EXCEPTION 'Cannot sell item % % because it does not exists', item_number, item_code;
 	END IF;
 
-	-- Check if one of us is the owner
-        SELECT INTO current_owner i_clientkey FROM t_items WHERE (i_inumkey = item_number AND i_alphakey = item_code);
+	-- Ensure the specified client actually exists
+	IF NOT(SELECT EXISTS (SELECT 1 FROM t_clients WHERE cl_clname = client)) THEN
+		RAISE EXCEPTION 'Cannot sell item % % to client % because no client with that name exists', item_number, item_code, client;
+	END IF;
 
-	IF NOT(SELECT EXISTS (SELECT 1 FROM v_us WHERE cl_name = current_owner LIMIT 1)) THEN
-		RAISE EXCEPTION 'Cannot sell item % % because it belongs to %', item_number, item_code, current_owner;
+	-- Ensure one of us is the owner
+        SELECT INTO current_owner i_clientkey FROM t_items WHERE (i_inumkey = item_number AND i_ialphakey = item_code);
+
+	IF NOT(SELECT EXISTS (SELECT 1 FROM v_us WHERE cl_clname = current_owner LIMIT 1)) THEN
+		RAISE EXCEPTION 'Cannot sell item % % because it belongs to %, not one of our museums', item_number, item_code, current_owner;
+	END IF;
+
+	-- Ensure that current owner and seller are not the same
+	IF (client = current_owner) THEN
+		RAISE EXCEPTION 'Cannot sell item % % because the client already owns it', item_number, item_code;
+	END IF;
+
+	-- Warn the user when an item is sold at midnight (they probably didn't specify a full date and time)
+	IF ((SELECT EXTRACT(HOUR FROM CAST(sale_date AS timestamp)) = 0) 
+		AND (SELECT EXTRACT(MINUTE FROM CAST(sale_date AS timestamp)) = 0) 
+		AND (SELECT EXTRACT(SECOND FROM CAST(sale_date AS timestamp)) = 0)) THEN
+		RAISE NOTICE 'Warning: selling item % % at midnight, are you sure you specified a date and time?', item_number, item_code;
 	END IF;
 
 END;
