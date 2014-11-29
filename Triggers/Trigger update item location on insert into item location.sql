81ec06c2854ddc7d9a9f021e7c540305299c3849
Iain Workman <ipw969@tux1.usask.ca>
2014-11-29 16:52:08 -0600

    Finished the trigger to update the last location on inserts into item location

--- Triggers/Trigger update item location on insert into item location.sql
+++ Triggers/Trigger update item location on insert into item location.sql
@@ -1,20 +1,31 @@
 create or replace function insertIntoItemLocationsUpdate() returns trigger as $updateItemLocationsOnInsert$
 begin
-update t_itemLocations set ilo_ilodatetime_end = new.ilo_ilodatetime_start
+update t_item_locations set ilo_ilodatetime_end = new.ilo_ilodatetime_start
 where 
         ilo_inumkey = new.ilo_inumkey
         AND
         ilo_ialphakey = new.ilo_ialphakey
         AND
-        ilo_iclientkey_item = new.ilo_iclientkey_item
+        ilo_clientkey_item = new.ilo_clientkey_item
         AND
         ilo_ilodatetime_start = 
-                (select ilo_ilodatetime_start from t_item_locations where ilo_inumkey = new.ilo_inumkey order by ilo_ilodatetime_start desc));
+                (
+			select 
+			MAX(ilo_ilodatetime_start)
+			from t_item_locations 
+			where 
+				ilo_inumkey = new.ilo_inumkey 
+				AND 
+				ilo_ialphakey = NEW.ilo_ialphakey 
+				AND 
+				ilo_clientkey_item = NEW.ilo_clientkey_item 
+		);
                 --This subquery selects only the latest record for this item to update.
+RETURN NEW;
 END;
 $updateItemLocationsOnInsert$ LANGUAGE plpgsql;
 
-DROP TRIGGER update_item_location ON t_item_locations;
-CREATE TRIGGER update_item_location AFTER INSERT ON
-t_item_location FOR EACH ROW EXECUTE PROCEDURE updateItemLocationsOnInsert();
+DROP TRIGGER IF EXISTS update_item_location ON t_item_locations;
+CREATE TRIGGER update_item_location BEFORE INSERT ON
+t_item_locations FOR EACH ROW EXECUTE PROCEDURE insertIntoItemLocationsUpdate();
                 
