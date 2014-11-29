bb7163ec6067e53a211dbbe2191d95b9d7b461a2
iainworkman <iain.workman@googlemail.com>
2014-11-29 16:27:38 -0600

    Altered updateItemLocationsOnInsert trigger
    Changed it to update last item even if it's end date is not null.

--- Triggers/Trigger update item location on insert into item location.sql
+++ Triggers/Trigger update item location on insert into item location.sql
@@ -1,8 +1,20 @@
-create function insertIntoItemLocationsUpdate() returns trigger as $updateItemLocationsOnInsert$
+create or replace function insertIntoItemLocationsUpdate() returns trigger as $updateItemLocationsOnInsert$
 begin
 update t_itemLocations set ilo_ilodatetime_end = new.ilo_ilodatetime_start
 where 
-        ilo_inumkey = new.ilo_inumkey and ilo_ilodatetime_end is null
-        and ilo_ilodatetime_start = 
+        ilo_inumkey = new.ilo_inumkey
+        AND
+        ilo_ialphakey = new.ilo_ialphakey
+        AND
+        ilo_iclientkey_item = new.ilo_iclientkey_item
+        AND
+        ilo_ilodatetime_start = 
                 (select ilo_ilodatetime_start from t_item_locations where ilo_inumkey = new.ilo_inumkey order by ilo_ilodatetime_start desc));
                 --This subquery selects only the latest record for this item to update.
+END;
+$updateItemLocationsOnInsert$ LANGUAGE plpgsql;
+
+DROP TRIGGER update_item_location ON t_item_locations;
+CREATE TRIGGER update_item_location AFTER INSERT ON
+t_item_location FOR EACH ROW EXECUTE PROCEDURE updateItemLocationsOnInsert();
+                
