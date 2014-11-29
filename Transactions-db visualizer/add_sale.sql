c098fa956de13b41cf3a7ad8a1241c58ac0139c1
iainworkman <iain.workman@googlemail.com>
2014-11-29 16:03:49 -0600

    Rnamed Variables in add_sale
    Changed the variable names in add_sale to make it clearer when running them.

--- Transactions-db visualizer/add_sale.sql
+++ Transactions-db visualizer/add_sale.sql
@@ -1,9 +1,9 @@
 BEGIN;
 SELECT 
 add_sale(
-        CAST(${NUMBER||null||BigDecimal||nullable ds=22 dt=NUMERIC }$ AS inumkey), 
-        CAST(${CODE||null||String||nullable ds=10 dt=VARCHAR }$ AS ialphakey), 
+        CAST(${numkey||null||BigDecimal||nullable ds=22 dt=NUMERIC }$ AS inumkey), 
+        CAST(${alphakey||null||String||nullable ds=10 dt=VARCHAR }$ AS ialphakey), 
         CAST(${client_name || null || String || nullable ds=10 dt=VARCHAR}$ AS clname), 
         CAST(${sale_date|| null ||Timestamp||nullable ds=7 dt=TIMESTAMP }$ AS itdatetime), 
-        CAST(${value || null || Float || nullable ds=10 dt=NUMERIC }$ AS itgross)
+        CAST(${sale_value || null || Float || nullable ds=10 dt=NUMERIC }$ AS itgross)
 );
