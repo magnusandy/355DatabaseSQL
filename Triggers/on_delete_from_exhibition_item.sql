e3951ebf06032ab4a23ee905e810af34be9b7470
Iain Workman <ipw969@tux3.usask.ca>
2014-11-30 15:33:36 -0600

    Redacted the on_delete_from_exhibition_item trigger, but kept the body of the function in comments for future reference

--- Triggers/on_delete_from_exhibition_item.sql
+++ Triggers/on_delete_from_exhibition_item.sql
@@ -1,8 +1,39 @@
+-- This trigger doesn't work, and isn't really feasible. Keeping this here for reference
+
+/*
 create or replace function updateLocationOnItemRemovedFromExhibition() returns trigger as $updateLocOnRemoveFromExhibit$
 DECLARE
 previous_location locname;
 next_location locname;
+previous_enddate ilodatetime;
+next_startdate ilodatetime;
 begin
+	-- Get the last location enddate prior to this
+	SELECT into previous_enddate max(ilo_ilodatetime_end) FROM t_item_locations
+	WHERE
+	(
+		ilo_inumkey = OLD.exi_inumkey
+		AND
+		ilo_ialphakey = OLD.exi_ialphakey
+		AND
+		ilo_clientkey_item = OLD.exi_clientkey
+		AND
+		ilo_ilodatetime_end < OLD.exi_exidate_start
+	);	
+
+	-- Get the start time in the location after this
+	SELECT into next_startdate min(ilo_ilodatetime_start) FROM t_item_locations
+	WHERE
+	(
+		ilo_inumkey = OLD.exi_inumkey
+		AND
+		ilo_ialphakey = OLD.exi_ialphakey
+		AND
+		ilo_clientkey_item = OLD.exi_clientkey
+		AND
+		ilo_ilodatetime_start > OLD.exi_exidate_end
+	);
+
 	-- Get the previous location this item was in before being moved for this exhibition
 	SELECT into previous_location ilo_locname FROM t_item_locations
 	WHERE
@@ -13,8 +44,9 @@ begin
 		AND
 		ilo_clientkey_item = OLD.exi_clientkey
 		AND
-		ilo_ilodatetime_end = OLD.exi_exidate_start
+		ilo_ilodatetime_end = previous_enddate 
 	);
+	RAISE NOTICE 'numkey = % alpha = %, clientkey = %, enddate = %', OLD.exi_inumkey, OLD.exi_ialphakey, OLD.exi_clientkey, OLD.exi_exidate_end;
 	RAISE NOTICE 'prev_location = %', previous_location;
 	
 	-- Get the next location where this item will be after this exhibition
@@ -27,7 +59,7 @@ begin
 		AND
 		ilo_clientkey_item = OLD.exi_clientkey
 		AND
-		ilo_ilodatetime_start = OLD.exi_exidate_end
+		ilo_ilodatetime_start = next_startdate
 	);
 	RAISE NOTICE 'Next location = %', next_location;
 
@@ -43,24 +75,11 @@ begin
 	-- Check if item is in a location after this
 	IF(next_location IS NOT NULL ) THEN
 		-- If it is we need to check if it was in storage before this
-		RAISE NOTICE 'Next location is not null';
+		RAISE NOTICE 'In a location after this';
 		IF(previous_location = 'Storage') THEN
 			RAISE NOTICE 'Was in storage before this';
 			-- If it is we need to update the storage date end to the next time it's moved somewhere
-			UPDATE t_item_locations SET ilo_ilodatetime_end = 
-			(	
-				SELECT ilo_datetime_start FROM t_item_locations
-				WHERE
-				(
-					ilo_inumkey = OLD.exi_inumkey
-					AND
-					ilo_ialphakey = OLD.exi_ialphakey
-					AND
-					ilo_clientkey_item = OLD.exi_clientkey
-					AND
-					ilo_ilodatetime_start = OLD.exi_exidate_end
-				)
-			)
+			UPDATE t_item_locations SET ilo_ilodatetime_end = next_startdate
 			WHERE
 			(
 				ilo_inumkey = OLD.exi_inumkey
@@ -97,7 +116,7 @@ begin
 		END IF;
 	ELSE
 		-- If it isn't then we need to check if it was in storage before
-		RAISE NOTICE 'Is in a location after this';
+		RAISE NOTICE 'Not in a location after this';
 		IF(previous_location = 'Storage') THEN
 			RAISE NOTICE 'Was in storage before';
 			-- If it was we can just extend this to indefinite by nulling the end date
@@ -144,3 +163,4 @@ $updateLocOnRemoveFromExhibit$ LANGUAGE plpgsql;
 DROP TRIGGER IF EXISTS update_location_on_removed_from_exhibition ON t_exhibition_items;
 CREATE TRIGGER update_location_on_removed_from_exhibition AFTER DELETE ON
 t_exhibition_items FOR EACH ROW WHEN (OLD.exi_exidate_start > current_date) EXECUTE PROCEDURE updateLocationOnItemRemovedFromExhibition();
+*/
