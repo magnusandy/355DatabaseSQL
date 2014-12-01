110895e43c7d8d23c334af377eae58085576bd7d
PhlexPlexico <etc114@mail.usask.ca>
2014-11-30 19:20:12 -0600

    Update Transaction Insert item into internal exhibition.sql
    Cleaned up a little, to make it readable.

--- DbVisualizerScripts/Transactions/Transaction Insert item into internal exhibition.sql
+++ DbVisualizerScripts/Transactions/Transaction Insert item into internal exhibition.sql
@@ -1,2 +1,9 @@
 select insertItemIntoInternalExhibition(
-${numkey||null||Float}$, ${alphakey||null||String}$, ${exhibitionname||null||String}$, ${exstartdate||null||Date}$, ${iteminexhibitionstart||null||Date}$, ${iteminexhibitionend||null||Date}$, ${location||null||String}$, ${clientlocationkey||null||String}$);
+  ${numkey||null||Float}$, 
+  ${alphakey||null||String}$, 
+  ${exhibitionname||null||String}$, 
+  ${exstartdate||null||Date}$, 
+  ${iteminexhibitionstart||null||Date}$, 
+  ${iteminexhibitionend||null||Date}$,
+  ${location||null||String}$, ${clientlocationkey||null||String}$
+);
