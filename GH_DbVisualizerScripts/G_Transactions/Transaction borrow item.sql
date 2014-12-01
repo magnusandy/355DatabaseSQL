ee7104a8703d61f3bdb5c7512f872b72872e3b36
unknown <rajlaforge@hotmail.ca>
2014-11-30 19:42:59 -0600

    Added the borrow and loan scripts for db visualizer

--- GH_DbVisualizerScripts/G_Transactions/Transaction borrow item.sql
+++ GH_DbVisualizerScripts/G_Transactions/Transaction borrow item.sql
@@ -1,1 +1,9 @@
-select donothing();
+BEGIN;
+select
+add_borrow(
+CAST(${NUMBER||null||BigDecimal||nullable ds=22 dt=NUMERIC }$ AS inumkey), 
+CAST(${CODE||null||String||nullable ds=10 dt=VARCHAR }$ AS ialphakey),
+CAST(${client_name || null || String || nullable ds=10 dt=VARCHAR}$ AS clname), 
+CAST(${borrow_date|| null ||Timestamp||nullable ds=7 dt=TIMESTAMP }$ AS itdatetime), 
+CAST(${due_date|| null ||Timestamp||nullable ds=7 dt=TIMESTAMP }$ AS itdatetime)
+);        
