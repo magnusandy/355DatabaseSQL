da935730444f402ffc9f092592a4ecea64681d0d
Iain Workman <iain.workman@googlemail.com>
2014-12-01 14:33:38 -0600

    Edited transactions for item_transactions

--- GH_DbVisualizerScripts/G_Transactions/Transaction add_purchase.sql
+++ GH_DbVisualizerScripts/G_Transactions/Transaction add_purchase.sql
@@ -1,7 +1,19 @@
 select add_purchase(
-	${numkey||null||Float}$, 
-	${alphakey||null||String}$, 
-	${buyer||null||String}$, 
-	${exstartdate||null||Date}$, 
-	${cost||(null)||Float}$
+        CAST(${numkey||null||BigDecimal||nullable ds=22 dt=NUMERIC }$ AS inumkey), 
+        CAST(${alphakey||null||String||nullable ds=10 dt=VARCHAR }$ AS ialphakey), 
+        CAST(${buyer_name || null || String || nullable ds=10 dt=VARCHAR}$ AS clname), 
+        CAST(${purchase_date|| null ||Timestamp||nullable ds=7 dt=TIMESTAMP }$ AS itdatetime), 
+        CAST(${purchase_value || null || Float || nullable ds=10 dt=NUMERIC }$ AS itgross)
 );
+
+SELECT * FROM t_item_transactions WHERE
+it_inumkey = CAST(${numkey||null||BigDecimal||nullable ds=22 dt=NUMERIC }$ AS inumkey);
+
+SELECT i_inumkey, i_ialphakey, i_clientkey FROM t_items WHERE
+i_inumkey = CAST(${numkey||null||BigDecimal||nullable ds=22 dt=NUMERIC }$ AS inumkey);
+
+SELECT * FROM t_item_locations WHERE
+ilo_inumkey = CAST(${numkey||null||BigDecimal||nullable ds=22 dt=NUMERIC }$ AS inumkey);
+
+SELECT * FROM t_exhibition_items WHERE
+exi_inumkey = CAST(${numkey||null||BigDecimal||nullable ds=22 dt=NUMERIC }$ AS inumkey);
--- GH_DbVisualizerScripts/G_Transactions/Transaction loan item.sql
+++ GH_DbVisualizerScripts/G_Transactions/Transaction loan item.sql
@@ -6,4 +6,5 @@ CAST(${CODE||null||String||nullable ds=10 dt=VARCHAR }$ AS ialphakey),
 CAST(${client_name || null || String || nullable ds=10 dt=VARCHAR}$ AS clname), 
 CAST(${loan_date|| null ||Timestamp||nullable ds=7 dt=TIMESTAMP }$ AS itdatetime), 
 CAST(${due_date|| null ||Timestamp||nullable ds=7 dt=TIMESTAMP }$ AS itdatetime)
-);        
+);
+       
