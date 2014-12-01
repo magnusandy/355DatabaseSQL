301c0faf001603160e1c32d2e8b52dca42257cc5
knames <okenso@gmail.com>
2014-11-30 18:53:34 -0600

    removed duplicate files
    edited for readability, removed duplicate files (the transaction files
    were really dbvis scripts, located in the correct folder already.)

--- DbVisualizerScripts/Transactions/Transaction add_purchase.sql
+++ DbVisualizerScripts/Transactions/Transaction add_purchase.sql
@@ -1,1 +1,7 @@
-select add_purchase(${numkey||null||Float}$, ${alphakey||null||String}$, ${buyer||null||String}$, ${exstartdate||null||Date}$, ${cost||(null)||Float}$);
+select add_purchase(
+	${numkey||null||Float}$, 
+	${alphakey||null||String}$, 
+	${buyer||null||String}$, 
+	${exstartdate||null||Date}$, 
+	${cost||(null)||Float}$
+);
