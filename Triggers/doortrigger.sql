2273e47030d7805f121908bed5f8a87e00fa239b
knames <okenso@gmail.com>
2014-11-30 18:19:28 -0600

    readability, dead files
    removed files that are not possible, made some files easier to read

--- Triggers/doortrigger.sql
+++ Triggers/doortrigger.sql
@@ -1,7 +1,8 @@
 CREATE FUNCTION ins_oppo_door() RETURNS TRIGGER AS $oppo_door$
 BEGIN
 IF((SELECT COUNT(*) FROM t_location_doors WHERE  lodor_locname_entrance = new.lodor_locname_exit and lodor_locname_exit = new.lodor_locname_entrance) = 0) THEN
-INSERT INTO t_location_doors (lodor_locname_entrance, lodor_clientkey_entrance, lodor_locname_exit, lodor_clientkey_exit) VALUES (new.lodor_locname_exit, new.lodor_clientkey_entrance, new.lodor_locname_entrance, new.lodor_clientkey_exit);
+INSERT INTO t_location_doors (lodor_locname_entrance, lodor_clientkey_entrance, lodor_locname_exit, lodor_clientkey_exit) 
+		VALUES (new.lodor_locname_exit, new.lodor_clientkey_entrance, new.lodor_locname_entrance, new.lodor_clientkey_exit);
 END IF;
 RETURN NEW;
 END;
